242ce21b32cd73e1f0db058186d1dff197fe5a49
Patrick Choi <ptrckchoi@gmail.com>
2022-11-09 16:02:12 -0800

    working on restful routes

--- routing_project/config/routes.rb
+++ routing_project/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
+
+  get 'users', to: 'users#index'
+  post 'users', to: 'users#create'
+  get 'users/new', to: 'users#new'
+  get 'users/:id/edit', to: 'users#edit'
+  get 'users/:id', to: 'users#show'
+  patch 'users/:id', to: 'users#update'
+
 
   # Defines the root path route ("/")
   # root "articles#index"
