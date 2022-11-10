0eb376a389b16f3ec03b7e307b44878b75bd96fd
Patrick Choi <ptrckchoi@gmail.com>
2022-11-10 10:35:29 -0800

    almost finished with project

--- routing_project/app/controllers/users_controller.rb
+++ routing_project/app/controllers/users_controller.rb
@@ -6,7 +6,15 @@ class UsersController < ApplicationController
     end
 
     def create
-        render json: params
+        # render json: params
+        user = User.new(params.require(:user).permit(:name, :email))
+         # replace the `user_attributes_here` with the actual attribute keys
+        # user.save!
+        if user.save
+            render json: user
+          else
+            render json: user.errors.full_messages, status: 422
+        end
     end
 
     def show
