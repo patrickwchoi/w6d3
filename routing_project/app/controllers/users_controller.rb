5de946f0d01425898c4e7c04984a60cdcf485cce
Patrick Choi <ptrckchoi@gmail.com>
2022-11-09 17:13:11 -0800

    middle of phase 3

--- routing_project/app/controllers/users_controller.rb
+++ routing_project/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
-        # users = User.all
-        # render json: users
+        # render plain: "I'm in the index action!"
+        users = User.all
+        render json: users
     end
 end
